6af14ebef07165b93ef04a90b7c081e2b1601600
János Gönye <janos.gonye@gmail.com>
2019-04-20 15:36:18 +0200

    add category to transaction's json

--- app/transactions/models.py
+++ app/transactions/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 from app.db import db
+from app.categories.models import Category
 from app.utils import datetime2timestamp
 
 class Transaction(db.Model):
@@ -17,11 +18,18 @@ class Transaction(db.Model):
 	updated_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
 	category_id = db.Column(db.Integer, db.ForeignKey('categories.id', ondelete='cascade'), nullable=False)
 
+	@property
+	def category(self):
+		category = Category.query.filter_by(id=self.category_id).all()
+		if len(category) == 0:
+			return None
+		return category[0]
+
 	def as_dict(self):
 		return {
 			'id': self.id,
 			'amount': self.amount,
 			'processed_at': datetime2timestamp(self.processed_at),
 			'comment': self.comment,
-			'category_id': self.category_id,
+			'category': self.category.as_dict(),
 		}
