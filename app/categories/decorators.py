3bb7cee8fb5114894b7d88f586db387c33779388
János Gönye <janos.gonye@gmail.com>
2019-06-13 17:15:58 +0200

    bugfix | handle if request method is not get but param == '*'

--- app/categories/decorators.py
+++ app/categories/decorators.py
@@ -15,8 +15,11 @@ def get_category_or_404(f):
 		# that belong to 'current_user'
 		special_path = unquote(url_for("transactions.get_transactions", cat_id="*"))
 		param = request.view_args['cat_id']
-		if param == '*' and request.path == special_path and request.method == "GET":
-			return f(current_user, '__all__')
+		if param == '*' and request.path == special_path:
+			if request.method == "GET":
+				return f(current_user, '__all__')
+			elif request.method != "GET":
+				return js('Category not found.', 404)
 		cat = Category.query.filter_by(id=param, user_id=current_user.id).first()
 		if not cat:
 			return js('Category not found.', 404)
