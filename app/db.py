75cf2c1f13aa0699197856fc12be03aeab7ba271
János Gönye <janos.gonye@gmail.com>
2019-07-28 14:58:53 +0200

    add code to make migration work

--- app/db.py
+++ app/db.py
@@ -1,7 +1,9 @@
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
 
 def init_app(app):
+	migrate = Migrate(app, db)
 	db.init_app(app)
 	with app.app_context():
 		db.create_all()
