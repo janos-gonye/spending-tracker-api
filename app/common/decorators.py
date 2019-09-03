b1472fed36d6aa243146126aa69a92010db5e5b1
János Gönye <janos.gonye@gmail.com>
2019-09-03 14:48:01 +0200

    extend the 'jsonify_view' with support for optional return values

--- app/common/decorators.py
+++ app/common/decorators.py
@@ -7,6 +7,13 @@ def jsonify_view(f):
 
     @wraps(f)
     def decorated(*args, **kwargs):
-        msg, status_code = f(*args, **kwargs)
+        res = f(*args, **kwargs)
+        l = len(res)
+        if l == 2:
+            msg, status_code = res
+            return js(msg, status_code)
+        if l == 3:
+            msg, status_code, kwargs = res
+            return js(msg, status_code, **kwargs)        
         return js(msg, status_code)
     return decorated
