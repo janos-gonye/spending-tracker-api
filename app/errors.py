f8e12815c3dc77cf49a6b5533d0c6c5d6713b4e3
János Gönye <janos.gonye@gmail.com>
2019-09-03 17:27:32 +0200

    handle json validation error

--- app/errors.py
+++ app/errors.py
@@ -1,5 +1,5 @@
 from app.common import js
-from app.common.exceptions import ValidationError
+from app.common.exceptions import JsonValidationError, ValidationError
 
 
 def init_app(app):
@@ -7,3 +7,7 @@ def init_app(app):
     @app.errorhandler(ValidationError)
     def handle_validation_error(e):
         return e.message, e.status_code
+
+    @app.errorhandler(JsonValidationError)
+    def handle_json_validation_error(e):
+        return js(e.message, e.status_code)
