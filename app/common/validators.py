71ef53fcd97df4879777076bfdf0ddc5bfc80991
János Gönye <janos.gonye@gmail.com>
2019-09-05 10:27:03 +0200

    raise 'ValidationError's in 'app.common.validators', too

--- app/common/validators.py
+++ app/common/validators.py
@@ -1,5 +1,7 @@
 import re
 
+from app.common.exceptions import ValidationError
+
 EMAIL_REGEX = r'^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$'
 
 
@@ -12,9 +14,11 @@ def validate_password(password, min_length=6, max_length=50,
        contain_lower and not re.search(r"[a-z]", password) or \
        contain_upper and not re.search(r"[A-Z]", password) or \
        contain_number and not re.search(r"[0-9]", password):
-        return False
-    return True
+        raise ValidationError(
+            f"Password's length must be at least {min_length} characters. "
+            "Must contain number, upper and lower case letters.")
 
 
 def validate_email(email):
-    return bool(re.match(EMAIL_REGEX, email))
+    if not bool(re.match(EMAIL_REGEX, email)):
+        raise ValidationError('Invalid email address.')
