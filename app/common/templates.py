b7db50d12ee3972561cc1d57027aef111c82e7d7
János Gönye <janos.gonye@gmail.com>
2019-09-15 11:34:16 +0200

    generator 'token_as_arg_validator_template' implemented

--- app/common/templates.py
+++ app/common/templates.py
@@ -1,8 +1,10 @@
+from time import time
 from functools import wraps
 
 from flask import request
 
 from app.common.exceptions import ValidationError
+from app.common.token import decode_token
 
 
 def json_validator_template(validator=None):
@@ -16,5 +18,24 @@ def json_validator_template(validator=None):
             if validator:
                 validator(data, *args, **kwargs)
             return f(data, *args, **kwargs)
+        return decorated
+    return decorator
+
+
+def token_as_arg_validator_template(validator=None):
+    def decorator(f):
+
+        @wraps(f)
+        def decorated(*args, **kwargs):
+            token = request.args.get('token')
+            if not token:
+                raise ValidationError('Token missing.')
+            payload = decode_token(token)
+            if not payload:
+                raise ValidationError('Token invalid.')
+            expires_at = payload.get('expiresAt')
+            if expires_at and float(expires_at) < time():
+                raise ValidationError('Token expired.', 401)
+            return f(payload, *args, **kwargs)
         return decorated
     return decorator
