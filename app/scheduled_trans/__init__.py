e6ac0652841ebece0b5f44c36cf52fd89d13a150
János Gönye <janos.gonye@gmail.com>
2020-01-12 13:11:10 +0100

    add scheduled transactions module with blueprint

---
+++ app/scheduled_trans/__init__.py
@@ -0,0 +1,10 @@
+from flask import Blueprint
+
+
+def init_app(app):
+    app.register_blueprint(scheduled_trans_blueprint,
+                           url_prefix='/api/scheduled-trans')
+
+
+scheduled_trans_blueprint = Blueprint('scheduled-trans', __name__)
+import app.scheduled_trans.routes
