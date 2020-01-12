b3901408d9f491102a41af0d99f7852b56c30c24
János Gönye <janos.gonye@gmail.com>
2020-01-12 15:02:53 +0100

    add 'get_scheduled_trans_or_404' decorator permission management

--- app/scheduled_trans/decorators.py
+++ app/scheduled_trans/decorators.py
@@ -8,7 +8,7 @@ from app.scheduled_trans.models import ScheduledTransaction
 def get_scheduled_trans_or_404(f):
     @wraps(f)
     def decorated(current_user, *args, **kwargs):
-        scheduled_trans = ScheduledTransaction.query.filter_by(
+        scheduled_trans = current_user.scheduled_transactions.query.filter_by(
             id=request.view_args['scheduled_trans_id']).first()
         if not scheduled_trans:
             return 'Scheduled transaction not found.', 404
